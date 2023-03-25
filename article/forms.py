396346e730c0466294fcbe09ad8088fe89adcba0
JIT <jit@discovermyfranchise.com>
2023-03-25 14:47:35 +0530

    create story version 1

--- article/forms.py
+++ article/forms.py
@@ -4,4 +4,4 @@ from .models import Article
 class StoryForm(ModelForm):
     class Meta:
         model = Article
-        fields = ['title','content','author','language','image']
+        fields = ['title','content','author','language','category','image']
